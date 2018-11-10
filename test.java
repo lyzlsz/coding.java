82a9d9058e527918c821eb74dca93d671bd66070
lsz <1812969441@qq.com>
2018-11-10 21:22:22 +0800

    exchange

---
+++ test.java
@@ -0,0 +1,12 @@
+public class test{
+	public static void main(String[] args){
+		int a=10;
+		int b=20;
+		int temp=0;
+		temp=a;
+		a=b;
+		b=temp;
+		System.out.println(a);
+		System.out.println(b);
+	}
+}
