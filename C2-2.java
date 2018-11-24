47609284c26bb32d7601be56cc51a995e646edb8
lsz <1812969441@qq.com>
2018-11-24 22:08:31 +0800

    test

---
+++ C2-2.java
@@ -0,0 +1,13 @@
+public class C2-2{
+	
+	public static void main(String args[]){
+		float x,y,z;
+		x=94.3f;y=23.9f;z=x/y;
+		System.out.println("x="+x);
+		System.out.println("y="+y);
+		System.out.println("z="+z);
+	}
+}
+		
+		
+		
