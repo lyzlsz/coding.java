3af30e98590e16eb506564e777c099ffec793f9a
lsz <1812969441@qq.com>
2018-11-24 22:12:09 +0800

    test

---
+++ C2_3.java
@@ -0,0 +1,16 @@
+public class C2_3
+{
+	
+	public static void main(String agrs[]){
+		char c1,c2,c3,c4;
+		c1='H';
+		c2='\\';
+		c3='\115';   //115是M的(77)的八进制
+		c4='\101';   //101是A的(65)的八进制
+		System.out.print(c1);
+		System.out.print(c2);
+		System.out.print(c3);
+		System.out.print(c4);
+	}
+}
+		
