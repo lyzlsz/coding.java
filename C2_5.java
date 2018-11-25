9cb46243e27fc8fec98c4baf6018d85ef0852ca2
lsz <1812969441@qq.com>
2018-11-25 21:42:30 +0800

    test

---
+++ C2_5.java
@@ -0,0 +1,20 @@
+public class C2_5{
+	
+	public static void main(String[] args){
+		int x,y,z,a,b;
+		a=9;
+		b=2;
+		x=a%b;
+		y=2+ ++a;
+		z=7+ --b;
+		System.out.println("x="+x);
+		System.out.println("y="+y);
+		System.out.println("z="+z);
+		
+		
+		
+		
+	}
+	
+}
+
