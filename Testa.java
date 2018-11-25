a4ddf23781bb442e37a2d08c7b25e591d5104333
lsz <1812969441@qq.com>
2018-11-25 21:36:03 +0800

    test

---
+++ Testa.java
@@ -0,0 +1,20 @@
+
+
+public class Testa{
+	
+	public static void main(String[] args){
+		boolean x,y,z;
+		int a=89,b=20;
+		x=(a>b);
+		y=(a!=b);
+		z=(a+b==44);
+		System.out.println("x "+x);
+		System.out.println("y "+y);
+		System.out.println("z "+z);
+	 	
+		
+		
+	}
+	
+}
+
