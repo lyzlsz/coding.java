52080985ca7d70eb009236681c0af670cb02e5ba
lsz <1812969441@qq.com>
2018-11-27 15:01:24 +0800

    test

---
+++ C3_1.java
@@ -0,0 +1,16 @@
+public class C3_1{
+	public static void main(String[] agrs){
+		int grade=86;
+		if(grade>=80){
+			System.out.print("优秀,成绩是: ");
+			System.out.println(grade);
+		}
+		else if(grade>=60 || grade <80){
+			System.out.print("及格,成绩是: ");
+			System.out.println(grade);
+		}
+		else{
+			System.out.println("不及格");
+	}
+	}
+}
