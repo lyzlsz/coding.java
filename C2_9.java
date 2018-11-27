eeb9debdac9b0c903f112fbd9b7ceb8366f54d0d
lsz <1812969441@qq.com>
2018-11-27 14:44:20 +0800

    test

---
+++ C2_9.java
@@ -0,0 +1,16 @@
+public class C2_9{
+	public static void main(String[] args){
+		boolean x,y,z,a,b;
+		a='b'>'N';
+		b='A'!='A';
+		x=(!a);
+		y=(a&&b);//条件与
+		z=(a&b);//&按位与 
+		System.out.println("a="+a);
+		System.out.println("b="+b);
+		System.out.println("x="+x);
+		System.out.println("y="+y);
+		System.out.println("z="+z);
+	}
+}
+		
