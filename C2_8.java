69378673f9e2b1b7d1ee65cf8903c50b62aee315
lsz <1812969441@qq.com>
2018-11-27 14:42:36 +0800

    test

---
+++ C2_8.java
@@ -0,0 +1,14 @@
+public class C2_8{
+	public static void main(String[] agrs){
+		boolean x,y;
+		double a,b;
+		a=12.897;
+		b=345.6;
+		x=(a!=b);
+		y=(a==b);
+		System.out.println("(a>b)="+(a>b));
+		System.out.println("(a<b)="+(a<b));
+		System.out.println("x\t"+x);
+		System.out.println("y\t"+y);
+	}
+}
