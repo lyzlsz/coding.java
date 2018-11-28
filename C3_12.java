d08975a7096ceb8323879c95ef661f8ad0c178c3
lsz <1812969441@qq.com>
2018-11-28 14:09:27 +0800

    test

---
+++ C3_12.java
@@ -0,0 +1,18 @@
+package com.bittech;
+
+/**
+ * Author:weiwei
+ * Creat:${Date}
+ **/
+public class C3_12 {
+    public static void main(String[] args) {
+        int i,sum;
+        for (i=1,sum=0;i<=10;i++)
+            sum +=i;
+            System.out.println("sum=" + sum);
+        }
+    }
+
+
+
+
