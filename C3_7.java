7368210cceb00d25fa0a45498c622cc02a61e09c
lsz <1812969441@qq.com>
2018-11-28 14:01:52 +0800

    test

---
+++ C3_7.java
@@ -0,0 +1,19 @@
+package com.bittech;
+
+/**
+ * Author:weiwei
+ * Creat:${Date}
+ **/
+public class C3_7 {
+    public static void main(String[] args){
+        int x,y,z,a,b;
+        a=1;
+        b=2;
+        x=(a>b)?a:b;
+        y=(a!=b)?a:b;
+        z=(a<b)?a:b;
+        System.out.print("\tx="+x);
+        System.out.print("\ty="+y);
+        System.out.print("\tz="+z);
+    }
+}
