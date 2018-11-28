d3ced43dcfe7fbe5f4fe72307e8e088298f8ff33
lsz <1812969441@qq.com>
2018-11-28 14:05:34 +0800

    test

---
+++ C3_10.java
@@ -0,0 +1,15 @@
+package com.bittech;
+
+/**
+ * Author:weiwei
+ * Creat:${Date}
+ **/
+public class C3_10 {
+    public static void main(String[] agrs){
+        int sum=0,odd;
+        for(odd=1;odd<=100;odd+=2){
+            sum+=odd;
+        }
+        System.out.println("sum="+sum+"  odd="+odd);
+    }
+}
