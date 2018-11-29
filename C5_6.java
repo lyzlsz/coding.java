59c2ed544d31b1fb89ad6d839397ebb34e3961f4
lsz <1812969441@qq.com>
2018-11-29 09:27:39 +0800

    test

---
+++ C5_6.java
@@ -0,0 +1,11 @@
+class A1{
+	int x=27;
+	private int z=10;
+}
+class C5_6 extends A1{
+	public static void main(String[] args){
+		C5_6 p=new C5_6();//创建一个C5_6类的P的对象
+		System.out.println("p.x="+p.x);
+		//System.out.println("p.z="+p.z);
+	}
+}
