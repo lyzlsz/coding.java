da6139a387ce219958de8c1c6d93edbcc0fc2694
lsz <1812969441@qq.com>
2018-12-29 22:39:04 +0800

    声明过期

---
+++ TestNEW.java
@@ -0,0 +1,23 @@
+package com.bittech;
+
+/**
+ * Author:weiwei
+ * Creat:2018/12/29
+ **/
+
+//class Person{
+//    @Deprecated
+//    public Person(){}
+//    public Person(String name){}
+//    @Deprecated
+//    public void fun(){}
+//}
+//public class TestNEW {
+//    public static void main(String[] args) {
+//        Person person = new Person();
+//        person.fun();
+//        person = new Person("");
+//
+//    }
+//
+//}
