0775fd2f7f5f2d805959478c9d053c2cbc4b6098
lsz <1812969441@qq.com>
2018-12-30 19:28:31 +0800

    多线程

---
+++ Thread16.java
@@ -0,0 +1,24 @@
+package com.bittech;
+
+/**
+ * Author:weiwei
+ * Creat:2018/12/30
+ **/
+//public class Thread16 {
+//    public static void main(String[] args) {
+//        Runnable runnable= new PrintRunnable();
+//        for(int i=0;i<10;i++){
+//            new Thread(runnable,"Thread-"+i).start();
+//        }
+//    }
+//}
+//class PrintRunnable implements Runnable{
+//    private int i;
+//    @Override
+//    public void run() {
+//        System.out.println();
+//
+//
+//
+//    }
+//}
