03e0b67b8b9ed477e747b6d07ce88d48ff9cc6ca
lsz <1812969441@qq.com>
2018-12-31 15:08:16 +0800

    方法引用---引用对象方法

---
+++ Test2.java
@@ -0,0 +1,19 @@
+package com.bittech;
+
+/**
+ * Author:weiwei
+ * Creat:2018/12/31
+ **/
+//方法引用
+    //引用对象方法
+    //String中toUppercase()方法为对象方法
+//@FunctionalInterface
+// interface IUtil <R>{
+//    public R switchPara();
+//}
+//public class Test2 {
+//    public static void main(String[] args) {
+//        IUtil<String> iu = "hello" :: toUpperCase; //进行方法引用
+//        System.out.println(iu.switchPara());
+//    }
+//}
