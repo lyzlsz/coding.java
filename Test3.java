bdc682f38e0f439230396be9e51a61a523495d24
lsz <1812969441@qq.com>
2018-12-31 15:10:26 +0800

    方法引用---引用类中普通方法

---
+++ Test3.java
@@ -0,0 +1,18 @@
+package com.bittech;
+
+/**
+ * Author:weiwei
+ * Creat:2018/12/31
+ **/
+//方法引用
+    //引用类中普通方法
+//@FunctionalInterface
+//interface IUtil <R,P>{
+//    public R compare(P p1,P p2);
+//}
+//public class Test3 {
+//    public static void main(String[] args) {
+//        IUtil <Integer,String> iu = String :: compareTo;
+//        System.out.println(iu.compare("刘","王"));
+//    }
+//}
