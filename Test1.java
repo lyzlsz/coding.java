034c28febb3d4a8c2e9f18af4237a1461c21741c
lsz <1812969441@qq.com>
2018-12-31 15:06:00 +0800

    方法引用----引用静态方法

---
+++ Test1.java
@@ -0,0 +1,21 @@
+package com.bittech;
+
+/**
+ * Author:weiwei
+ * Creat:2018/12/31
+ **/
+//方法引用
+    //引用静态方法
+    //String类valueOf方法
+//@FunctionalInterface   //这是一个函数式编程接口,只允许有一个方法
+//interface IUtil<P,R>{
+//    public R switchPara(P p);
+//}
+//public class Test1 {
+//    public static void main(String[] args) {
+//        IUtil<Integer,String> iu= String ::valueOf;  //进行方法引用
+//        String str = iu.switchPara(1000);//相当于调用了String.valueOf(1000)
+//        System.out.println(str.length());
+//    }
+//}
+//就相当于给方法起了个别名
