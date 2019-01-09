77058bfbcd1f38cf6925b58c7b41ede68a8f6dc1
lsz <1812969441@qq.com>
2019-01-09 16:33:00 +0800

    关于Map中的key

---
+++ TestMap5.java
@@ -0,0 +1,44 @@
+package Map;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Author:weiwei
+ * Creat:2019/1/8
+ **/
+//关于Map中的Key
+//class Person{
+//    private String name;
+//    private Integer  age;
+//
+//    public Person(String name,Integer age){
+//        this.name=name;
+//        this.age = age;
+//    }
+//    @Override
+//    public String toString(){
+//        return "Person{"+"age="+age+",name"+name+"\'"+"}";
+//    }
+//    public String getName(){
+//        return name;
+//    }
+//    public void setName(String name){
+//        this.name = name;
+//    }
+//    public Integer getAge(){
+//        return age;
+//    }
+//    public void setAge(Integer age){
+//        this.age = age;
+//    }
+//}
+//public class TestMap5 {
+//    public static void main(String[] args) {
+//        Map<Person,String> map = new HashMap<>();
+//        map.put(new Person("Jack",15),"JK");
+//        map.put(new Person("Mike",20),"MK");
+//        System.out.println(map.get(new Person("Jack",15)));
+//        System.out.println(map.get(new Person("Mike",20)));
+//    }
+//}
