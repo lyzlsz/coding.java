81bf32cadac522e4eb0401ed2500b5a11ac334d5
lsz <1812969441@qq.com>
2019-01-14 22:07:10 +0800

    notifyAll方法

---
+++ TestnotifyAll.java
@@ -0,0 +1,75 @@
+package Test;
+
+/**
+ * Author:weiwei
+ * Creat:2019/1/14
+ **/
+
+//生产者消费者模型
+    //notifyAll()方法
+
+class MyThread implements Runnable {
+    private boolean flag;
+    private Object obj;
+
+    public MyThread (boolean flag,Object obj){
+        this.flag =flag;
+        this.obj =obj;
+    }
+    public void waitMethod(){
+        synchronized(obj){
+            try{
+                while(true){
+                    System.out.println("wait()方法开始"+Thread.currentThread().getName());
+                    obj.wait();
+                    System.out.println("wait()方法结束"+Thread.currentThread().getName());
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    public void notifyMethod(){
+        synchronized(obj){
+            try{
+                System.out.println("notifyAll()方法开始"+Thread.currentThread().getName());
+                obj.notifyAll();
+                System.out.println("notifyAll()方法结束"+Thread.currentThread().getName());
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public void run() {
+        if(flag){
+            this.waitMethod();
+        }else {
+            this.notifyMethod();
+        }
+    }
+}
+public class TestnotifyAll {
+    public static void main(String[] args) {
+        Object object = new Object();
+        MyThread waitThread1= new MyThread(true,object);
+        MyThread waitThread2= new MyThread(true,object);
+        MyThread waitThread3= new MyThread(true,object);
+        MyThread notifyThread= new MyThread(false,object);
+        Thread thread1=new Thread(waitThread1,"wait线程A");
+        Thread thread2=new Thread(waitThread2,"wait线程B");
+        Thread thread3=new Thread(waitThread3,"wait线程C");
+        Thread thread4=new Thread(notifyThread,"notify线程");
+        thread1.start();
+        thread2.start();
+        thread3.start();
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        thread4.start();
+        System.out.println("main方法结束");
+    }
+}
