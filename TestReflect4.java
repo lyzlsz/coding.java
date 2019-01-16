22d5c01dcf70a4331263f232873411a224abf3f9
lsz <1812969441@qq.com>
2019-01-16 16:54:13 +0800

    反射调用构造--取得类中所有构造信息

---
+++ TestReflect4.java
@@ -0,0 +1,26 @@
+package com.bittech;
+
+import java.lang.reflect.Constructor;
+
+/**
+ * Author:weiwei
+ * Creat:2019/1/16
+ **/
+
+//反射调用构造
+    //取得类中所有构造信息
+// class Person{
+//     public Person(){}
+//     public Person(String name){}
+//     public Person(String name,int age){}
+//}
+//public class TestReflect4 {
+//    public static void main(String[] args) {
+//        Class <?> cls=Person.class;
+//        //取得类中所有构造
+//        Constructor <?>[] constructors =cls.getConstructors();
+//        for(Constructor<?> constructor  : constructors){
+//            System.out.println(constructor);
+//        }
+//    }
+//}
