cb3e1730a91a3bb7167d932fd4053ecad9268e86
lsz <1812969441@qq.com>
2019-01-29 20:46:26 +0800

    在方法上使用Annotation

---
+++ TestAnnotation2.java
@@ -0,0 +1,34 @@
+package Annotation;
+
+import java.io.Serializable;
+import java.lang.annotation.Annotation;
+
+/**
+ * Author:weiwei
+ * description:在方法上使用Annotation
+ * Creat:2019/1/29
+ **/
+
+//@SuppressWarnings("serial")
+//@Deprecated
+//class Member implements Serializable {
+//    @Deprecated
+//    @Override
+//    public String toString(){
+//        return super.toString();
+//    }
+//}
+//
+//public class TestAnnotation2 {
+//    public static void main(String[] args) {
+//        Annotation [] ant = new Annotation[0];
+//        try{
+//            ant = Member.class.getMethod("toString").getAnnotations();
+//        } catch (NoSuchMethodException e) {
+//            e.printStackTrace();
+//        }
+//        for(Annotation  a: ant ){
+//            System.out.println(a);
+//        }
+//    }
+//}
