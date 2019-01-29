a61314a6264744270caf493f7c3526a2b30e5443
lsz <1812969441@qq.com>
2019-01-29 20:07:19 +0800

    取得定义在类上的Annotation

---
+++ TestAnnotation1.java
@@ -0,0 +1,24 @@
+package Annotation;
+
+import java.io.Serializable;
+import java.lang.annotation.Annotation;
+
+/**
+ * Author:weiwei
+ * description:反射与Annotation
+ * Creat:2019/1/29
+ **/
+
+
+//@SuppressWarnings("serial")
+//@Deprecated
+//class Member implements Serializable {}
+//public class TestAnnotation1 {
+//    public static void main(String[] args) {
+//        Annotation [] ant = Member.class.getAnnotations();
+//        for(Annotation  a: ant ){
+//            System.out.println(a);
+//        }
+//    }
+//
+//}
