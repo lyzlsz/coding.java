cda250a5462c1efa7a72b187a778ede6d5c05c2a
lsz <1812969441@qq.com>
2019-01-31 21:21:08 +0800

    观察Java Heap OOM

---
+++ TestOOMObject.java
@@ -0,0 +1,27 @@
+package com.bittech;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Author:weiwei
+ * description:观察Java Heap OOM
+ * Creat:2019/1/31
+ **/
+//public class TestOOMObject {
+//    private int stackLength = 1;
+//    public void stackLeak() {
+//        stackLength ++;
+//        stackLeak();
+//    }
+//
+//    static class OOMObject{}
+//    public static void main(String[] args) {
+//        List<OOMObject> list=new ArrayList<>();
+//        while(true){
+//            list.add(new OOMObject());
+//        }catch (Throwable e){
+//            System.out.println("Stack Length:"+OOMObject );
+//        }
+//    }
+//}
