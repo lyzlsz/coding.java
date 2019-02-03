4b1312a70548d5a92bc3ff46d2b95aecac617878
lsz <1812969441@qq.com>
2019-02-03 21:09:40 +0800

    趣味编程--乘法口诀表

---
+++ Ch1_2.java
@@ -0,0 +1,17 @@
+package interestingprogram;
+
+/**
+ * Author:weiwei
+ * description:
+ * Creat:${Date}
+ **/
+public class Ch1_2 {
+    public static void main(String[] args) {
+        for(int i=1;i<=9;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(" "+i+"*"+j+"="+(i*j));
+            }
+            System.out.println();
+        }
+    }
+}
