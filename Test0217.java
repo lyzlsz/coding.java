62dcfd7b8d6d583a5748700c00e081e27bae8aae
lsz <1812969441@qq.com>
2019-02-17 19:48:34 +0800

    example

---
+++ Test0217.java
@@ -0,0 +1,31 @@
+package Thread;
+
+/**
+ * Author:weiwei
+ * description:
+ * Creat:2019/2/17
+ **/
+//abstract class A{
+//    public A(){
+//        this.print();
+//    }
+//    public abstract void print();
+//}
+//class B extends A{
+//    private int num = 100;
+//    public B(int num){
+//        super();
+//        this.num =num;
+//    }
+//    @Override
+//    public void print() {
+//        System.out.println(this.num);
+//    }
+//}
+//
+//public class Test0217 {
+//    public static void main(String[] args) {
+//        new B(30).print();
+//    }
+//
+//}
