097c6afcf7938718eea24fb89258b33e037088f2
lsz <1812969441@qq.com>
2019-03-10 21:15:47 +0800

    数据库--商品表

---
+++ shop.sql
@@ -0,0 +1,3 @@
+* 商品goods(商品编号goods_id，商品名goods_name, 单价unitprice, 商品类别category, 供应商provider)
+*客户customer(客户号customer_id,姓名name,住址address,邮箱email,性别sex，身份证card_id)
+*购买purchase(购买订单号order_id,客户号customer_ id,商品号goods_ id,购买数量nums)
