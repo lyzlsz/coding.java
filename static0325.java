8215109c29316e6779fe2e84d5e38816fea961cc
lsz <1812969441@qq.com>
2019-03-25 21:31:55 +0800

    static关键字

---
+++ static0325.java
@@ -0,0 +1,29 @@
+//package Teststatic;
+//
+///**
+// * Author:weiwei
+// * description:static关键字
+// * Creat:2019/3/25
+// **/
+//
+//class Person{
+//    String Country = "中国";
+//    String name;
+//    int age;
+//
+//    public void getPersonInfo(){
+//        System.out.println("姓名:"+this.name+"年龄:"+this.age+"国家:"+this.Country);
+//    }
+//}
+//public class static0325 {
+//    public static void main(String[] args) {
+//        Person p1 = new Person();
+//        p1.name="张三";
+//        p1.age=22;
+//        Person p2 = new Person();
+//        p2.name="李四";
+//        p2.age=23;
+//        p1.getPersonInfo();
+//        p2.getPersonInfo();
+//    }
+//}
