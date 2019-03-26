8a2722f462b622e1dbe91aa58ecea8ecca248d9d
lsz <1812969441@qq.com>
2019-03-26 14:29:52 +0800

    this调用本类方法

---
+++ Testthis326.java
@@ -0,0 +1,30 @@
+package Testthis;
+
+/**
+ * Author:weiwei
+ * description:this调用本类方法
+ * Creat:2019/3/26
+ **/
+
+//class Person{
+//    private String name;
+//    private int age;
+//
+//    public Person(String name,int age){
+//        this.name = name;
+//        this.age = age;
+//        this.print();//this调用普通方法
+//    }
+//    public String getPersonInfo(){
+//        return "姓名:"+name+",年龄:"+age;
+//    }
+//    public void print(){
+//        System.out.println("*************");
+//    }
+//}
+//public class Testthis326 {
+//    public static void main(String[] args) {
+//        Person per = new Person("Mike",22);
+//        System.out.println(per.getPersonInfo());
+//    }
+//}
