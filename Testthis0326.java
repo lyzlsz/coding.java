1d8d41d0e4f8fccbcb8c5f8fe89f16ddb0ec671d
lsz <1812969441@qq.com>
2019-03-26 14:28:27 +0800

    this调用本类属性

---
+++ Testthis0326.java
@@ -0,0 +1,27 @@
+package Testthis;
+
+/**
+ * Author:weiwei
+ * description:this调用本类属性
+ * Creat:2019/3/26
+ **/
+
+//class Person{
+//    private String name;
+//    private int age;
+//
+//    public Person(String name,int age){
+//        this.name = name;
+//        this.age = age;
+//    }
+//
+//    public String getPersonInfo(){
+//        return "姓名:"+name+",年龄:"+age;
+//    }
+//}
+//public class Testthis0326 {
+//    public static void main(String[] args) {
+//        Person per = new Person("jack",22);
+//        System.out.println(per.getPersonInfo());
+//    }
+//}
