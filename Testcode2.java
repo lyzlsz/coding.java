071a20791afc00820123bc33882d14d9dfb0b9fc
lsz <1812969441@qq.com>
2019-03-28 13:32:16 +0800

    构造块

---
+++ Testcode2.java
@@ -0,0 +1,22 @@
+package com.bittech;
+
+/**
+ * Author:weiwei
+ * description:构造块
+ * Creat:2019/3/26
+ **/
+//class Person{
+//    {
+//        //定义在类中,不加任何修饰符,构造块
+//        System.out.println("1. Person类的构造块");
+//    }
+//    public Person(){
+//        System.out.println("2.Person类的构造方法");
+//    }
+//}
+//public class Testcode2 {
+//    public static void main(String[] args) {
+//        new Person();
+//        new Person();
+//    }
+//}
