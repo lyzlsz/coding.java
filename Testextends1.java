4604e6b866f161c5cfc617bb77cb8281fece462e
lsz <1812969441@qq.com>
2019-03-29 22:17:36 +0800

    继承

---
+++ Testextends1.java
@@ -0,0 +1,40 @@
+package Testextends;
+
+/**
+ * Author:weiwei
+ * description:继承
+ * Creat:2019/3/27
+ **/
+
+//class Person{
+//    private String name;
+//    private int age;
+//
+//    public String getName(){
+//        return name;
+//    }
+//
+//    public void setName(String name){
+//        this.name = name;
+//    }
+//
+//    public int getAge(){
+//        return age;
+//    }
+//
+//    public void setAge(int age) {
+//        this.age = age;
+//    }
+//}
+//class Student extends Person{
+//
+//}
+
+//public class Testextends1 {
+//    public static void main(String[] args) {
+//        Student student = new Student();
+//        student.setName("Steven");
+//        student.setAge(26);
+//        System.out.println("姓名:"+student.getName()+",年龄:"+student.getAge());
+//    }
+//}
