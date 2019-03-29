c17587a9a6d0cc244454562639ca9c2c4cca8307
lsz <1812969441@qq.com>
2019-03-29 22:20:49 +0800

    继承--子类对象创建

---
+++ Testextends3.java
@@ -0,0 +1,24 @@
+//package Testextends;
+//
+///**
+// * Author:weiwei
+// * description:继承--子类对象创建
+// * Creat:201/3/27
+// **/
+//
+//class Person{
+//    public Person(){
+//        System.out.println("Person类对象产生");
+//    }
+//}
+//class Student extends Person{
+//    public Student(){
+//        super();//此语句在无参时写与不写一样
+//        System.out.println("Student类对象产生");
+//    }
+//}
+//public class Testextends3 {
+//    public static void main(String[] args) {
+//        new Student();
+//    }
+//}
