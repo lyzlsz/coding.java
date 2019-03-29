d61d2d362de213dc745a056793e1ff67fc49db5f
lsz <1812969441@qq.com>
2019-03-29 22:21:59 +0800

    显式继承与隐式继承

---
+++ Testextends4.java
@@ -0,0 +1,32 @@
+package Testextends;
+
+/**
+ * Author:weiwei
+ * description显式继承与隐式继承
+ * Creat:2019/3/27
+ **/
+//
+//class Person{
+//    private String name;
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//}
+//class Student extends Person{
+//    public void fun(){
+//        System.out.println(getName());
+//    }
+//}
+//public class Testextends4 {
+//    public static void main(String[] args) {
+//        Student student = new Student();
+//        student.setName("Steven");
+//        System.out.println(student.getName());
+//        student.fun();
+//    }
+//}
