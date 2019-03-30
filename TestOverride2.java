6e68132b3f68daf7cb9dc970ea497b9318944bba
lsz <1812969441@qq.com>
2019-03-30 21:38:14 +0800

    使用super调用父类属性

---
+++ TestOverride2.java
@@ -0,0 +1,27 @@
+package TestOverride;
+
+/**
+ * Author:weiwei
+ * description:使用super调用父类属性
+ * Creat:2019/3/28
+// **/
+//class Person{
+//    public String info = "爸爸";
+//}
+//class Student  extends Person{
+//    public String info = "儿子";
+//
+//    public void print(){
+//        //不找本类中的属性
+//        System.out.println(super.info);
+//        System.out.println(this.info);
+//    }
+//
+//    public static class print {
+//    }
+//}
+//public class TestOverride2 {
+//    public static void main(String[] args) {
+//        new Student.print();
+//    }
+//}
