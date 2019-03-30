9fb64ef8579f82692a6347a0915ac0bbb05c612e
lsz <1812969441@qq.com>
2019-03-30 21:40:40 +0800

    简单的覆写

---
+++ TestOverride1.java
@@ -0,0 +1,27 @@
+package TestOverride;
+
+/**
+ * Author:weiwei
+ * description:简单的覆写
+ * Creat:2019/3/28
+// **/
+//
+//class Person{
+//    public void print(){
+//        System.out.println("1.[Person]类的print方法");
+//    }
+//}
+//class Student extends Person{
+//    public void print(){
+//        System.out.println("2.[Student]类的print方法");
+//    }
+//
+//    public static class print {
+//    }
+//}
+//public class TestOverride1 {
+//    public static void main(String[] args) {
+//        new Student.print();
+//    }
+//}
+//
