866c96531e53a0a6015e675a67c42b357971bc69
lsz <1812969441@qq.com>
2019-03-30 21:43:08 +0800

    向上转型

---
+++ TestOverride3.java
@@ -0,0 +1,24 @@
+package TestOverride;
+
+/**
+ * Author:weiwei
+ * description:向上转型
+ * Creat:2019/3/29
+ **/
+//
+//class Person{
+//    public void print(){
+//        System.out.println("1.我是爸爸!");
+//    }
+//}
+//class Student extends Person{
+//    public void print(){
+//        System.out.println("2.我是儿子!");
+//    }
+//}
+//public class TestOverride3 {
+//    public static void main(String[] args) {
+//        Person per = new Student();//向上转型
+//        per.print();
+//    }
+//}
