e6c52c05e04fa2035bd07729a3335b8a0f8a9684
lsz <1812969441@qq.com>
2019-03-31 19:44:46 +0800

    显式继承与隐式继承

---
+++ TestOverride4.java
@@ -0,0 +1,43 @@
+package TestOverride;
+
+/**
+ * Author:weiwei
+ * description:向下转型
+ * Creat:2019/3/29
+ **/
+//
+//class Person{
+//    public void print(){
+//        System.out.println("1.我是爸爸!");
+//    }
+//}
+//class Student extends Person{
+//    public void print(){
+//        System.out.println("2.我是儿子!");
+//    }
+//    public void fun(){
+//        System.out.println("只有儿子有!");
+//    }
+//}
+//public class TestOverride4 {
+//    public static void main(String[] args) {
+//        Person per = new Person();
+//        per.print();
+//        //这个时候父类能够调用的方法都是本类定义好的方法
+//        //所以并没有Student类中的fun()方法,那么只能够进行向下转型处理
+//        Student stu = (Student) per;
+//        stu.fun();
+//    }
+//}
+//
+//
+
+
+
+
+
+
+
+
+
+
