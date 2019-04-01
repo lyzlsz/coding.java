bb0f1967cd44c34307a85b8bf8b6b548ed87a906
lsz <1812969441@qq.com>
2019-04-01 23:21:40 +0800

    简单内部类

---
+++ TestInner1.java
@@ -0,0 +1,28 @@
+package Inner;
+
+/**
+ * Author:weiwei
+ * description:简单内部类
+ * Creat:2019/3/29
+// **/
+//
+//class Outer{
+//    private String msg = "Hello World!";
+//
+//    class Inner{
+//        public void print(){//定义一个内部类
+//            System.out.println(msg);//调用msg属性
+//        }
+//    }
+//    //在外部类中定义一个方法该方法负责产生内部类对象并且调用print()方法
+//    public void fun(){
+//        Inner in = new Inner();//内部类对象
+//        in.print();
+//    }
+//}
+//public class TestInner1 {
+//    public static void main(String[] args) {
+//        Outer outer = new Outer();//外部类对象
+//        outer.fun();//外部类方法
+//    }
+//}
