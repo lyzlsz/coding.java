3480c82aceec695dd7518f77d04b0ffcac3c2de8
lsz <1812969441@qq.com>
2019-04-02 16:34:24 +0800

    外部类可以通过内部类引用间接访问内部类元素

---
+++ TestInner5.java
@@ -0,0 +1,26 @@
+package Inner;
+
+/**
+ * Author:weiwei
+ * description:外部类可以通过内部类引用间接访问内部类元素
+ * Creat:2019/3/29
+ **/
+//
+//class Outter{
+//    void display(){
+//        //外部类访问内部元素,需要通过内部类引用来访问
+//        Inner inner = new Inner();
+//        inner.display();
+//    }
+//    class Inner{
+//        public void display(){
+//            System.out.println("I am InnerClass ");
+//        }
+//    }
+//}
+//public class TestInner5 {
+//    public static void main(String[] args) {
+//        Outter outter = new Outter();
+//        outter.display();
+//    }
+//}
