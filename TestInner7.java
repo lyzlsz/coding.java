f30ffeba810fee5c2f07656f0787e7ed5413c1fa
lsz <1812969441@qq.com>
2019-04-03 22:30:27 +0800

    方法内部类

---
+++ TestInner7.java
@@ -0,0 +1,27 @@
+package Inner;
+
+/**
+ * Author:weiwei
+ * description:方法内部类
+ * Creat:2019/3/29
+ **/
+//
+//class Outter {
+//    private int num;
+//    public void display(int test){
+//        class Inner{
+//            private void fun(){
+//                num++;
+//                System.out.println(num);
+//                System.out.println(test);
+//            }
+//        }
+//        new Inner().fun();
+//    }
+//}
+//public class TestInner7 {
+//    public static void main(String[] args) {
+//        Outter outter = new Outter();
+//        outter.display(20);
+//    }
+//}
