42e313897e92b5b06a0e3354ff83e64ff7a7596f
lsz <1812969441@qq.com>
2019-05-21 17:20:59 +0800

    MyClass

---
+++ MyClass.java
@@ -0,0 +1,14 @@
+package com.bittech.Test;
+
+/**
+ * package:com.bittech.Test
+ * Description:TODO
+ * @date:2019/5/21
+ * @Author:weiwei
+ **/
+public class MyClass {
+    static int i;
+    public static void main(String[] args) {
+        System.out.println(i);
+    }
+}
