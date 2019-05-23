c88d6a1c057efa58cf7bd0cdc1fde0e695cc310c
lsz <1812969441@qq.com>
2019-05-23 20:27:11 +0800

    Test

---
+++ Test0523_1.java
@@ -0,0 +1,19 @@
+package com.bittech.Test;
+
+/**
+ * package:com.bittech.Test
+ * Description:TODO
+ * @date:2019/5/23
+ * @Author:weiwei
+ **/
+//class Test{
+//    public static void hello(){
+//        System.out.println("hello");
+//    }
+//}
+//public class Test0523_1 {
+//    public static void main(String[] args) {
+//        Test test = null;
+//        test.hello();
+//    }
+//}
