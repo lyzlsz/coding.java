bdf4fcb02241fd7d9675b2f32b34c92a19d68747
lsz <1812969441@qq.com>
2019-06-09 23:55:12 +0800

    Packing and Unpack

---
+++ Test0609.java
@@ -0,0 +1,23 @@
+package com.bittech;
+
+/**
+ * package:com.bittech
+ * Description:TODO
+ *
+ * @date:2019/6/9 0009
+ * @Author:weiwei
+ **/
+public class Test0609 {
+    public static void main(String[] args) {
+        int i =10;
+        Integer k = 10;
+        Integer p = 10;
+        Integer j = new Integer(10);
+        Integer z = new Integer(10);
+        System.out.println(k == j);
+        System.out.println(i == j);
+        System.out.println(j == z);
+        System.out.println(p == k);
+
+    }
+}
