134ce188fd9c2b6ee35dde177d6fdd3f9f707974
lsz <1812969441@qq.com>
2019-06-09 23:51:36 +0800

    packing

---
+++ Packing.java
@@ -0,0 +1,20 @@
+package com.bittech;
+
+/**
+ * package:com.bittech
+ * Description:TODO
+ * @date:2019/6/9
+ * @Author:weiwei
+ **/
+public class Packing {
+    public static void main(String[] args) {
+//        Integer i = Integer.valueOf(55);
+////        int j = 55;
+////        System.out.println(i == j);
+
+        Integer x = Integer.valueOf(128);
+        Integer y = 128;
+        System.out.println(x == y);
+        System.out.println(x.equals(y));
+    }
+}
