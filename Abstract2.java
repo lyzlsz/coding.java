f582695f3bbd844264c3ab503f503516209ed121
lsz <1812969441@qq.com>
2019-06-30 22:13:33 +0800

    Abstractclass

---
+++ Abstract2.java
@@ -0,0 +1,30 @@
+package Abstract;
+
+/**
+ * package:Abstract
+ * Description:TODO
+ *
+ * @date:2019/6/27 0027
+ * @Author:weiwei
+ **/
+abstract class A{
+    public A(){//3.调用父类构造
+        this.print();//4.调用被子类覆写的方法
+    }
+    public abstract void print();
+}
+class B extends A{
+    private int num = 100;
+    public B(int num){//2.调用子类实例化对象
+        super();//3.隐含一行语句,实际要先调用父类构造
+        this.num = num;//7.为类中属性进行初始化
+    }
+    public void print(){//5.此时子类对象的属性还没有被初始化
+        System.out.println(this.num);//6.对应其数据类型的默认值
+    }
+}
+public class Test2 {
+    public static void main(String[] args) {
+        new B(30);//1.实例化对象
+    }
+}
