607fd2d8e481ec5bf07c661040a3a0e2a1427f80
lsz <1812969441@qq.com>
2019-07-18 23:05:26 +0800

    TestSet

---
+++ SetTest.java
@@ -0,0 +1,21 @@
+package Base2;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+/**
+ * package:Base2
+ * Description:TODO
+ *
+ * @date:2019/7/18 0018
+ * @Author:weiwei
+ **/
+public class SetTest {
+    public static void main(String[] args) {
+        Set<String> set = new TreeSet<>();
+        set.add("A");
+        set.add("B");
+        set.add("C");
+        System.out.println(set);
+    }
+}
