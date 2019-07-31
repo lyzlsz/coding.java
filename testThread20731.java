653456228ee22fcf19ad0b34d5f81729d74f29cf
lsz <1812969441@qq.com>
2019-07-31 22:54:58 +0800

    synchronized

---
+++ testThread20731.java
@@ -0,0 +1,24 @@
+package com;
+
+/**
+ * package:com
+ * Description:TODO
+ *
+ * @date:2019/7/31 0031
+ * @Author:weiwei
+ **/
+//class MyThread extends Thread{
+//    private int tickets = 10;
+//    @Override
+//    public void run(){
+//        while(this.tickets > 0){
+//            System.out.println("剩余票数:"+this.tickets--);
+//        }
+//    }
+//}
+//public class testThread2 {
+//    public static void main(String[] args) {
+//        new MyThread().start();
+//        new MyThread().start();;
+//    }
+//}
