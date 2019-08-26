024f5425ea5a26881f116b070f8c10ee78956715
lsz <1812969441@qq.com>
2019-08-26 23:29:00 +0800

    httpd--staticController

---
+++ StaticController.java
@@ -0,0 +1,21 @@
+package com.lsz;
+
+/**
+ * package:com.lsz
+ * Description:TODO
+ *
+ * @date:2019/8/18 0018
+ * @Author:weiwei
+ **/
+public class StaticController implements Controller {
+
+    @Override
+    public void doGet() {
+
+    }
+
+    @Override
+    public void doPost() {
+
+    }
+}
